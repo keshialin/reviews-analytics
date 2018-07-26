c1c13766a4f1160deb03cc83523079e2b4b5616c
林妘襄 <keshia@linyunxiangde-MacBook-Pro.local>
2018-07-26 14:36:26 +0800

    add progressbar

--- read.py
+++ read.py
@@ -1,11 +1,14 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		bar.update(count)
 print('檔案讀取完了，總共有{}筆資料'.format(len(data)))
 
 print(data[0])
@@ -32,6 +35,7 @@ print('一共有{}筆留言提到good'.format(len(good)))
 print(good[0])
 
 #文字計數
+start_time = time.time()
 wc = {} # word_count
 for d in data:
 	words = d.split()
@@ -44,6 +48,8 @@ for d in data:
 for word in wc:
 	if wc[word] > 1000000:
 		print(word, wc[word])
+end_time = time.time()
+print('花了{}秒'.format(end_time - start_time))
 
 while True:
 	word = input('請問你想查什麼字/[q]quit:')
